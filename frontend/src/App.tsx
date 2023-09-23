11790be6b5c26d9ce52c96ecd9b0a7dac79ccfe4
kenji-iwatani <kleversonkenji@gmail.com>
2023-09-23 19:10:32 -0300

    adiciona prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import defaultTheme from "./data/defaultTheme";
-import {ThemeProvider, Typography} from "@mui/material";
+import { ThemeProvider, Typography } from "@mui/material";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Typography sx={{color: "primary.main"}}>Teste</Typography>
+      <Typography sx={{ color: "primary.main" }}>Teste</Typography>
     </ThemeProvider>
   );
 }
