3cb3066c1e48cc8eb3ff3e111512b40888659c3b
iwataniKenji <kleversonkenji@gmail.com>
2023-10-23 10:14:26 -0300

    adiciona o type do Estoque

--- frontend/src/data/estoqueMock.ts
+++ frontend/src/data/estoqueMock.ts
@@ -1,32 +1,34 @@
-export const estoqueMock = [
+import { Estoque } from "../types/Estoque";
+
+export const estoqueMock: Estoque[] = [
   {
     id: 1,
     nome: "Produto 1",
     quantidade: 10,
-    createdAt: "2021-05-01T00:00:00.000Z",
+    createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
   {
     id: 2,
     nome: "Produto 2",
     quantidade: 20,
-    createdAt: "2021-05-02T00:00:00.000Z",
+    createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
   {
     id: 3,
     nome: "Produto 3",
     quantidade: 30,
-    createdAt: "2021-05-03T00:00:00.000Z",
+    createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
   {
     id: 4,
     nome: "Produto 4",
     quantidade: 40,
-    createdAt: "2021-05-04T00:00:00.000Z",
+    createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
   {
     id: 5,
     nome: "Produto 5",
     quantidade: 50,
-    createdAt: "2021-05-05T00:00:00.000Z",
+    createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
 ];
