154c94fa7bb2d98d86993e7667796b3428e8dcf9
iwataniKenji <kleversonkenji@gmail.com>
2023-10-23 10:22:58 -0300

    corrige questões de tipagem

--- frontend/src/data/estoqueMock.ts
+++ frontend/src/data/estoqueMock.ts
@@ -2,31 +2,31 @@ import { Estoque } from "../types/Estoque";
 
 export const estoqueMock: Estoque[] = [
   {
-    id: 1,
+    id: "1",
     nome: "Produto 1",
     quantidade: 10,
     createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
   {
-    id: 2,
+    id: "2",
     nome: "Produto 2",
     quantidade: 20,
     createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
   {
-    id: 3,
+    id: "3",
     nome: "Produto 3",
     quantidade: 30,
     createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
   {
-    id: 4,
+    id: "4",
     nome: "Produto 4",
     quantidade: 40,
     createdAt: new Date("2021-05-01T00:00:00.000Z"),
   },
   {
-    id: 5,
+    id: "5",
     nome: "Produto 5",
     quantidade: 50,
     createdAt: new Date("2021-05-01T00:00:00.000Z"),
