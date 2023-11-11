470d30431816f3ff8a2f4318176808d8ecd7bedb
iwataniKenji <kleversonkenji@gmail.com>
2023-11-11 13:13:30 -0300

    fix: corrige bloqueio por cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // TODO -> inserir no .env ou afins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
