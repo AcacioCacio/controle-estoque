ad73b9fea278ea3c097adbadf4e1c515e3a39c4c
iwataniKenji <kleversonkenji@gmail.com>
2023-11-19 22:28:26 -0300

    refactor: remove utilização de contexto para autenticação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,18 @@ import { SnackbarProvider } from "notistack";
 import { Box, ThemeProvider } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { ConfirmProvider } from "material-ui-confirm";
-import { AuthContextProvider } from "./contexts/AuthContext";
 import defaultTheme from "./data/defaultTheme";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <AuthContextProvider>
-        <SnackbarProvider autoHideDuration={6000}>
-          <ConfirmProvider>
-            <Box
-              sx={{ display: "flex", width: "100vw", height: "100vh", m: 0 }}
-            >
-              <Outlet />
-            </Box>
-          </ConfirmProvider>
-        </SnackbarProvider>
-      </AuthContextProvider>
+      <SnackbarProvider autoHideDuration={6000}>
+        <ConfirmProvider>
+          <Box sx={{ display: "flex", width: "100vw", height: "100vh", m: 0 }}>
+            <Outlet />
+          </Box>
+        </ConfirmProvider>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
