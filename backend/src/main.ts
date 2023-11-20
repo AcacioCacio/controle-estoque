84348bd83e6c0777364b1996ad2370b099411ee7
iwataniKenji <kleversonkenji@gmail.com>
2023-11-19 21:39:09 -0300

    fix: muda a origem do front no backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5000', // TODO -> inserir no .env ou afins
+    origin: 'http://localhost:3000', // TODO -> inserir no .env ou afins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
   });
 
   app.useGlobalPipes(
