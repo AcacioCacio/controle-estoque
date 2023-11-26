11378d0518494110686c0cae19de1656efc58356
iwataniKenji <kleversonkenji@gmail.com>
2023-11-26 20:25:22 -0300

    refactor: remove conteúdo não usado

--- frontend/src/services/movimentaApi.ts
+++ frontend/src/services/movimentaApi.ts
@@ -1,9 +1,5 @@
 import httpClient from "../lib/httpClient";
 
-// export type FirebaseReturn =
-//   | { message: string; error?: undefined }
-//   | { error: any; message?: undefined };
-
 async function findAll(): Promise<any[]> {
   const client = httpClient();
 
